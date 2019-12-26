ed191f2193a999eca2cd74b5584ec868845ac2b0
kshefchek <kshefchek@gmail.com>
2019-12-26 11:22:17 -0800

    pheno togo

---
+++ upper/README.md
@@ -0,0 +1,39 @@
+### Classification of upper ontology terms
+
+As a first test we aim to create to a multiclass classifier of the
+upper ontology terms in Upheno:
+
+    'UBERON:0001434PHENOTYPE': 'Skeletal system',
+    'UBERON:0002101PHENOTYPE': 'Limbs',
+    'UBERON:0001016PHENOTYPE': 'Nervous system',
+    'UBERON:0007811PHENOTYPE': 'Head or neck',
+    'MP:0005376': 'Metabolism/homeostasis',
+    'UBERON:0004535PHENOTYPE': 'Cardiovascular system',
+    'UBERON:0002416PHENOTYPE': 'Integument',
+    'UBERON:0004122PHENOTYPE': 'Genitourinary system',
+    'UBERON:0000970PHENOTYPE': 'Eye',
+    'UBERON:0001015PHENOTYPE': 'Musculature',
+    'MPATH:218PHENOTYPE': 'Neoplasm',
+    'UBERON:0001007PHENOTYPE': 'Digestive system',
+    'UBERON:0002405PHENOTYPE': 'Immune system',
+    'UBERON:0002390PHENOTYPE': 'Blood and blood-forming tissues',
+    'UBERON:0000949PHENOTYPE': 'Endocrine',
+    'UBERON:0001004PHENOTYPE': 'Respiratory system',
+    'UBERON:0001690PHENOTYPE': 'Ear',
+    'UBERON:0002384PHENOTYPE': 'Connective tissue',
+    'UBERON:0000323PHENOTYPE': 'Prenatal development or birth',
+    'GO:0040007PHENOTYPE': 'Growth',
+    'HP:0025142': 'Symptom',
+    'UBERON:0002224PHENOTYPE': 'Thoracic cavity',
+    'UBERON:0000310PHENOTYPE': 'Breast',
+    'HP:0001608': 'Voice',
+    'CL:0000000PHENOTYPE': 'Cellular'
+
+#### Project setup
+virtualenv venv -p /usr/bin/python3.7
+source venv/bin/activate
+pip install -r requirements.txt
+    
+#### Fetch data phenotype-go term associations from monarch solr
+venv/bin/python scripts/pheno-to-go.py -p phenotypes.txt
+
---
+++ upper/scripts/pheno-to-go.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+"""pheno-to-go.py
+
+Script for pulling phenotype to go term associations by joining
+gene to phenotype and gene to gene function associations
+
+Outputs a three column file in the format:
+HP:1    Gene:1    GO:1,GO:2,GO:3
+HP:2    Gene:1    GO:1,GO:4,GO:5
+
+Prints summary statistics:
+GO profiles per class,
+Maximum length GO profile for a class for zero paddingdownstream
+"""
+
+import argparse
+import requests
+
+solr = "https://solr-dev.monarchinitiative.org/solr/golr/select/"
+
+parser = argparse.ArgumentParser(description=__doc__,
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('--phenotypes', '-p', type=str, required=True,
+                    help='List of phenotypes')
+parser.add_argument('--annotations', '-a', required=False,
+                    default="./output.tsv", help='output annotations file')
+parser.add_argument('--go', '-g', required=False,
+                    default="./go-terms.tsv", help='output unique set of go terms')
+args = parser.parse_args()
+
+max_profile = 0
+counter = 0
+class_stats = {}
+output_fh = open(args.annotations, 'w')
+go_fh = open(args.go, 'w')
+all_go = set()
+
+# Iterate over phenotype classes and fetch GO profiles
+with open(args.phenotypes, 'r') as input_file:
+    for phenotype in input_file:
+        phenotype = phenotype.rstrip()
+        class_stats[phenotype] = 0
+
+        # Fetch associated genes
+        params = {
+            'wt': 'json',
+            'rows': 0,
+            'facet': 'true',
+            'facet.mincount': 1,
+            'facet.limit': 100000,
+            'facet.field': 'subject',
+            'json.nl': 'arrarr',
+            'q': '*:*',
+            'fq': [
+                'association_type:gene_phenotype',
+                'object_closure:"{}"'.format(phenotype),
+            ],
+        }
+        req = requests.get(solr, params)
+        resp = req.json()
+
+        for gene_facet in resp['facet_counts']['facet_fields']['subject']:
+            counter += 1
+            gene = gene_facet[0]
+            go_profile = set()
+            has_go = False
+
+            # Fetch associated genes
+            params = {
+                'wt': 'json',
+                'rows': 0,
+                'facet': 'true',
+                'facet.mincount': 1,
+                'facet.limit': 100000,
+                'facet.field': 'object_closure',
+                'json.nl': 'arrarr',
+                'q': '*:*',
+                'fq': [
+                    'association_type:gene_function',
+                    'subject:"{}"'.format(gene),
+                ],
+            }
+
+            req = requests.get(solr, params)
+            resp = req.json()
+
+            for go_facet in resp['facet_counts']['facet_fields']['object_closure']:
+                if go_facet[0].startswith('GO'):
+                    has_go = True
+                    go_profile.add(go_facet[0])
+                    all_go.add(go_facet[0])
+
+            if has_go:
+                class_stats[phenotype] += 1
+                output_fh.write("{}\t{}\t{}\n".format(phenotype, gene, ','.join(go_profile)))
+                if len(go_profile) > max_profile:
+                    max_profile = len(go_profile)
+
+            if counter != 0 and counter % 1000 == 0:
+                print("processed {} genes".format(counter))
+
+
+print(class_stats)
+print(max_profile)
+
+for go in all_go:
+    go_fh.write("{}\n".format(go))
+
+go_fh.close()
+output_fh.close()
